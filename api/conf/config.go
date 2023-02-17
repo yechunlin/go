5fc5da6aa39b01f0c39493b60dae35896dea4d25
ycl <321902878@qq.com>
2023-02-17 23:17:17 +0800

    111

--- api/conf/config.go
+++ api/conf/config.go
@@ -1,5 +1,10 @@
 package conf
 
+// release 生产 | debug 开发 | test 测试
+
+const APP_ENV = "debug"
+
+// 常量配置
 const (
 	MYSQLADDRESS = "124.70.187.23" //数据库连接IP
 
--- api/main.go
+++ api/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"api/conf"
 	"api/middleware/logger"
 	"api/route"
 	"api/util"
 	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
 	"os"
 	"strconv"
 	"syscall"
 )
 
 func main() {
+	//设置模式
+	gin.SetMode(conf.APP_ENV)
 	//初始化路由，启动服务
 	logger.WriteLogStr("初始化路由,启动服务")
 	server := endless.NewServer(":8081", route.RouteInit())
