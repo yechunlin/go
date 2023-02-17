74033c1a6a72547887828e8167d57e757fbf928c
ycl <321902878@qq.com>
2023-02-17 23:04:39 +0800

    111

--- api/main.go
+++ api/main.go
@@ -3,43 +3,19 @@ package main
 import (
 	"api/middleware/logger"
 	"api/route"
-	"context"
-	"log"
-	"net/http"
+	"api/util"
+	"github.com/fvbock/endless"
 	"os"
-	"os/signal"
+	"strconv"
 	"syscall"
-	"time"
 )
 
 func main() {
 	//初始化路由，启动服务
 	logger.WriteLogStr("初始化路由,启动服务")
-	//server := endless.NewServer(":8081", route.RouteInit())
-	//server.BeforeBegin = func(add string) {
-	//	util.FilePutContents("pid.txt", strconv.Itoa(syscall.Getpid()), os.O_CREATE|os.O_RDWR)
-	//}
-	//server.ListenAndServe()
-	r := route.RouteInit()
-	ser := &http.Server{
-		Addr:    ":8081",
-		Handler: r,
+	server := endless.NewServer(":8081", route.RouteInit())
+	server.BeforeBegin = func(add string) {
+		util.FilePutContents("pid.txt", strconv.Itoa(syscall.Getpid()), os.O_CREATE|os.O_RDWR)
 	}
-	go func() {
-		err := ser.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-
-		}
-	}()
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("server shutDown...")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	err := ser.Shutdown(ctx)
-	if err != nil {
-		log.Println("server exit err!")
-	}
-	log.Println("server exit!")
+	server.ListenAndServe()
 }
