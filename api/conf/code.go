9718f61c7b5a92bdc688e2413cef03bf37a56529
ycl <321902878@qq.com>
2023-02-17 18:20:29 +0800

    配置修改

---
+++ api/conf/code.go
@@ -0,0 +1,9 @@
+package conf
+
+const (
+	API_SERVER_SUCCESS = 0     //服务正常
+	API_SERVER_ERROR   = 10000 //服务异常错误码
+	API_ACCOUNT_ERROR  = 10001 //账号密码登录异常
+	API_TOKEN_EMPTY    = 10002 //token为空或未传
+
+)
--- api/conf/config.go
+++ api/conf/config.go
@@ -1,12 +1,14 @@
 package conf
 
 const (
-	API_SERVER_SUCCESS = 0     //服务正常
-	API_SERVER_ERROR   = 10000 //服务异常错误码
-	API_ACCOUNT_ERROR  = 10001 //账号密码登录异常
-	API_TOKEN_EMPTY    = 10002 //token为空或未传
-
 	MYSQLADDRESS = "124.70.187.23" //数据库连接IP
 
 	LOG_SAVE_DIR = "E:/go2/api/runtime/logs"
 )
+
+//MysqlConf
+/**
+数据库mysql
+*/
+type MysqlConf struct {
+}
