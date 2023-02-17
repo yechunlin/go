4ef0831e93e0141e41f0a89101f7b301e4c6a620
ycl <321902878@qq.com>
2023-02-17 22:41:45 +0800

    服务

--- api/main.go
+++ api/main.go
@@ -3,10 +3,43 @@ package main
 import (
 	"api/middleware/logger"
 	"api/route"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	//初始化路由，启动服务
 	logger.WriteLogStr("初始化路由,启动服务")
-	route.RouteInit()
+	//server := endless.NewServer(":8081", route.RouteInit())
+	//server.BeforeBegin = func(add string) {
+	//	util.FilePutContents("pid.txt", strconv.Itoa(syscall.Getpid()), os.O_CREATE|os.O_RDWR)
+	//}
+	//server.ListenAndServe()
+	r := route.RouteInit()
+	ser := &http.Server{
+		Addr:    ":8081",
+		Handler: r,
+	}
+	go func() {
+		err := ser.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Panicln("server shutDown...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := ser.Shutdown(ctx)
+	if err != nil {
+
+	}
+	log.Panicln("server exit!")
 }
--- api/route/routr.go
+++ api/route/routr.go
@@ -5,16 +5,10 @@ import (
 	"api/middleware/auth"
 	"api/middleware/logger"
 	recover2 "api/middleware/recover"
-	"api/util"
-	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
-	"os"
-	"strconv"
-	"syscall"
-	"time"
 )
 
-func RouteInit() {
+func RouteInit() *gin.Engine {
 	r := gin.New()
 	r.Use(recover2.MidError(), logger.FormateLogger(), auth.BaseApi())
 
@@ -22,12 +16,5 @@ func RouteInit() {
 	{
 		user.POST("/login", controller.Login)
 	}
-	server := endless.NewServer(":8081", r)
-	server.ReadHeaderTimeout = 3 * 60 * time.Second
-	server.WriteTimeout = 3 * 60 * time.Second
-	server.MaxHeaderBytes = 1 << 20
-	server.BeforeBegin = func(add string) {
-		util.FilePutContents("pid.txt", strconv.Itoa(syscall.Getpid()), os.O_CREATE|os.O_RDWR)
-	}
-	server.ListenAndServe()
+	return r
 }
